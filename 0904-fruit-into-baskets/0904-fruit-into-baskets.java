40ff2a9d6a8a6cc4654a491dbc5a01f5eef86ff7
naveenyadav-1 <naveenyadav9822@gmail.com>
2026-09-01 01:22:57 +0530

    Time: 59 ms (13.66%), Space: 69.9 MB (78.88%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,28 +1,29 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int l=0;
-        int r=0;
-        int maxlen=0;
-        int k=2;
-        while( r<fruits.length){
-            if(map.containsKey(fruits[r])){
-                map.put(fruits[r],map.get(fruits[r])+1);
-            }else{
-                map.put(fruits[r],1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        int k = 2;
+        while (r < fruits.length) {
+            if (map.containsKey(fruits[r])) {
+                map.put(fruits[r], map.get(fruits[r]) + 1);
+            } else {
+                map.put(fruits[r], 1);
             }
-            while(map.size()>k){
-                map.put(fruits[l],map.get(fruits[l])-1);
-                 
-                 if(map.get(fruits[l])==0){
+            if (map.size() > k) {
+                map.put(fruits[l], map.get(fruits[l]) - 1);
+
+                if (map.get(fruits[l]) == 0) {
                     map.remove(fruits[l]);
-                 }
-                 l++;
-            } 
-            maxlen=Math.max(maxlen,r-l+1);
-                r++; 
+                }
+                l++;
             }
-            return maxlen;
-            
-        } 
+            maxlen = Math.max(maxlen, r - l + 1);
+            r++;
+
+        }
+        return maxlen;
+
+    }
 }
