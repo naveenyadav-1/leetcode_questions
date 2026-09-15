41ddf3e3088e9ca288193892d307efb7015a690b
naveenyadav-1 <naveenyadav9822@gmail.com>
2026-09-15 23:58:57 +0530

    Time: 4 ms (67.21%), Space: 94.6 MB (42.95%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,20 +9,40 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-         Stack<Integer>st=new Stack<>();
-         ListNode temp=head;
-         while(temp!=null){
-            st.push(temp.val);
-            temp=temp.next;
-         }
-          temp=head;
-         while(temp!=null){
-            if(temp.val!=st.peek())return false;
-            st.pop();
-            temp=temp.next;
-         }
-         return true;
+    public ListNode reverseLinkedList(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr != null){
+            ListNode front=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=front;
+        }
+        return prev;
         
+
+    }
+    public boolean isPalindrome(ListNode head) {
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast.next!=null && fast.next.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+       }
+        ListNode secondHalf= reverseLinkedList(slow.next);
+        ListNode first=head;
+        ListNode second=secondHalf;
+        while(second !=null){
+            if(first.val!=second.val){
+                reverseLinkedList(slow.next);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+            // reverseLinkedList(slow.next);
+            // return true;
+        }
+        reverseLinkedList(slow.next);
+            return true;
     }
 }
